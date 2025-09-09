979f00b10baaefeb71e2790ffbd40e65f214d861
Eileen1222 <yihe.xiao@mail.utoronto.ca>
2025-09-09 14:31:09 -0400

    overload

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -4,7 +4,7 @@ public class Multiples {
 
     public static void main(String[] args) {
 
-        System.out.println(multiples(1000, 3, 5));
+        System.out.println(multiples());
     }
 
     public static int multiples(int n, int a, int b) {
@@ -12,6 +12,16 @@ public class Multiples {
         for (int i = 1; i < n; i++) {
 
             if (i % a == 0 || i % b == 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    public static int multiples() {
+        int counter = 0;
+        for (int i = 1; i < 1000; i++) {
+
+            if (i % 3 == 0 || i % 5 == 0) {
                 counter++;
             }
         }
